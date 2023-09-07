f610fbb1bd2c3f18f40fddce3cd4a045917ef2f7
goupengnb <105686737+goupengnb@users.noreply.github.com>
2023-09-07 13:42:46 +0800

    Update Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #增加主题
-echo "CONFIG_PACKAGE_luci-theme-$OWRT_THEME=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-$OWRT_THEME-config=y" >> .config
+echo "CONFIG_PACKAGE_luci-theme-$OpenWrt_THEME=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-$OpenWrt_THEME-config=y" >> .config
 echo "CONFIG_PACKAGE_luci=y" >> .config
 echo "CONFIG_LUCI_LANG_zh_Hans=y" >> .config

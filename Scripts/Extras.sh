a5781ae981e5c9884e1efcd4d28c766750ee3f2a
goupengnb <105686737+goupengnb@users.noreply.github.com>
2023-09-07 13:32:51 +0800

    更新 Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -3,3 +3,5 @@
 #增加主题
 echo "CONFIG_PACKAGE_luci-theme-$OWRT_THEME=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-$OWRT_THEME-config=y" >> .config
+echo "CONFIG_PACKAGE_luci=y" >> .config
+echo "CONFIG_LUCI_LANG_zh_Hans=y" >> .config
